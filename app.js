9d31fe11d93bdc21b3f9b757004b1a94d0ebfebe
isaackumi <isaackumi@github.io>
2019-10-24 19:13:37 +0000

    refactoring

--- app.js
+++ app.js
@@ -40,36 +40,6 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.set('trust proxy', true);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 // Static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -82,13 +52,13 @@ EventControllers
 ]
 );
 
-/*
+
 // return 404 for all other routes not specified in this file
 app.use('*',(req,res,next)=>{
     res.sendStatus('404');
 })
 
-*/
+
 const server =http.createServer(app)
 
 
